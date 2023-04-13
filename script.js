e316342da713da32933167fb32a3e3972acc3774
chandan yadav <cy75945@gmail.com>
2023-04-13 16:45:51 +0530

    c

--- script.js
+++ script.js
@@ -1,25 +1,18 @@
-//your JS code here. If required.
-// function next(){
-//     document.getElementById("circle1").style.backgroundColor="red";
-// }
-// function prev(){
-
-// }
 const circles = document.querySelectorAll(".circle");
 const prevBtn = document.querySelector("#prev");
 const nextBtn = document.querySelector("#next");
 const line=document.querySelectorAll(".line");
 
 let currentActive = 0;
-let currline=0;
+let currentLIne=0;
 
 prevBtn.addEventListener("click", () => {
   circles[currentActive - 1].classList.remove("active");
   currentActive--;
-  if(currline>0)
+  if(currentLIne>0)
   {  
-  line[currline-1].classList.remove("active");
-  currline--;
+  line[currentLIne-1].classList.remove("active");
+  currentLIne--;
   }
 
   if (currentActive === 0) {
@@ -32,9 +25,9 @@ prevBtn.addEventListener("click", () => {
 nextBtn.addEventListener("click", () => {
   circles[currentActive ].classList.add("active");
   currentActive++;
-  if(currline<line.length)
-  {  line[currline ].classList.add("active");
-    currline++;
+  if(currentLIne<line.length)
+  {  line[currentLIne ].classList.add("active");
+  currentLIne++;
 }
 
   if (currentActive === circles.length) {
