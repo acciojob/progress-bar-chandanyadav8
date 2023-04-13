2554c7d43c1e7ff47447e29b0222c514e9ccb1e8
chandan yadav <cy75945@gmail.com>
2023-04-13 16:34:18 +0530

    committed

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 //your JS code here. If required.
+// function next(){
+//     document.getElementById("circle1").style.backgroundColor="red";
+// }
+// function prev(){
+
+// }
+const circles = document.querySelectorAll(".circle");
+const prevBtn = document.querySelector("#prev");
+const nextBtn = document.querySelector("#next");
+const line=document.querySelectorAll(".line");
+
+let currentActive = 0;
+let currline=0;
+
+prevBtn.addEventListener("click", () => {
+  circles[currentActive - 1].classList.remove("active");
+  currentActive--;
+  if(currline>0)
+  {  
+  line[currline-1].classList.remove("active");
+  currline--;
+  }
+
+  if (currentActive === 0) {
+    prevBtn.disabled = true;
+  }
+
+  nextBtn.disabled = false;
+});
+
+nextBtn.addEventListener("click", () => {
+  circles[currentActive ].classList.add("active");
+  currentActive++;
+  if(currline<line.length)
+  {  line[currline ].classList.add("active");
+    currline++;
+}
+
+  if (currentActive === circles.length) {
+    nextBtn.disabled = true;
+  }
+
+  prevBtn.disabled = false;
+});
